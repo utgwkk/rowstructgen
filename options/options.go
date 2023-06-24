11dab0211bf1f94f24606f60629f650dd486c4ec
utgwkk <utagawakiki@gmail.com>
2023-06-24 14:47:20 +0900

    guess out file/package name from directory

--- options/options.go
+++ options/options.go
@@ -2,6 +2,10 @@ package options
 
 import (
 	"errors"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"github.com/gertd/go-pluralize"
 	"github.com/stoewer/go-strcase"
@@ -51,10 +55,37 @@ func New(
 		return nil, errors.New("table name not set")
 	}
 
+	s, err := os.Stat(outFilePath)
+	if err != nil {
+		return nil, err
+	}
+	if s.IsDir() {
+		opts.OutFilePath = guessOutFilePathFromTable(opts.OutFilePath, opts.Table)
+
+		if opts.PackageName == "" {
+			opts.PackageName = guessPackageNameFromOutFilePath(opts.OutFilePath)
+		}
+	}
+
 	return opts, nil
 }
 
 func guessStructNameFromTable(tableName string) string {
 	pluralized := pluralize.NewClient().Singular(tableName)
 	return strcase.UpperCamelCase(pluralized)
+}
+
+func guessOutFilePathFromTable(basePath, tableName string) string {
+	singularTableName := pluralize.NewClient().Singular(tableName)
+	return path.Join(basePath, singularTableName+".go")
+}
+
+func guessPackageNameFromOutFilePath(outFilePath string) string {
+	normalizedDir := filepath.Dir(filepath.ToSlash(outFilePath))
+	xs := strings.Split(normalizedDir, "/")
+	candidate := xs[len(xs)-1]
+	if candidate == "." || candidate == ".." {
+		return "main"
+	}
+	return candidate
 }
